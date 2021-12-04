3b2752b75c368ed9adad7ee4eb7a22b6a5d1885a
Diogo Ribeiro <diogjp10@gmail.com>
2021-12-04 17:40:19 +0000

    update

--- supplies-update.js
+++ supplies-update.js
@@ -4,7 +4,7 @@ const recal = require('./recal');
 const sql = 'UPDATE products_supplies SET ' +
     'supply_name=$1, qt=$2, qtvalue=$3, unit=$4 ' +
     'where supply_identity_id=$5 ' +
-    'AND product_id in (select id from products where userid = $6';
+    'AND product_id in (select id from products where userid = $6)';
 
 async function updateSupplies(pool, supply, userId) {
     var values = [supply.name, supply.qt,
