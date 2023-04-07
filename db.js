c2a5a32563f81d34651a0deced53df61589314fe
Diogo Ribeiro <diogjp10@gmail.com>
2023-04-07 11:37:34 +0100

    basic endpoints working fine

--- db.js
+++ db.js
@@ -1,34 +1,39 @@
-const { Pool } = require('pg');
+const mysql = require("promise-mysql");
 
-const suppliesAdd = require('./supplies-add');
-const suppliesDelete = require('./supplies-delete');
-const suppliesUpdate = require('./supplies-update');
+const suppliesAdd = require("./supplies-add");
+const suppliesDelete = require("./supplies-delete");
+const suppliesUpdate = require("./supplies-update");
 
-const productAdd = require('./product-add');
-const productGet = require('./product-get');
-const productGetEdit = require('./product-get-edit');
-const productDelete = require('./product-delete');
-const productUpdate = require('./product-update');
+const productAdd = require("./product-add");
+const productGet = require("./product-get");
+const productGetEdit = require("./product-get-edit");
+const productDelete = require("./product-delete");
+const productUpdate = require("./product-update");
 
-const recipeAdd = require('./recipes-add');
-const recipeDelete = require('./recipes-delete');
-const recipeGet = require('./recipes-get');
-const recipeRecal = require('./recal-recipes');
+const recipeAdd = require("./recipes-add");
+const recipeDelete = require("./recipes-delete");
+const recipeGet = require("./recipes-get");
+const recipeRecal = require("./recal-recipes");
 
-const recal = require('./recal');
+const recal = require("./recal");
 
-const deleteAllPro = require('./delete-all');
+const deleteAllPro = require("./delete-all");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const getDbConnection = async () => {
+  return await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+};
 
 function extractToken(req) {
-  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-    return req.headers.authorization.split(' ')[1];
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    return req.headers.authorization.split(" ")[1];
   } else if (req.query && req.query.token) {
     return req.query.token;
   }
@@ -38,14 +43,21 @@ function extractToken(req) {
 const getProductGetEdit = async (request, response) => {
   const id = extractToken(request);
   const productId = request.params.id;
-  var result = await productGetEdit.queryGetProductById(pool, id, productId);
+  const pool = await getDbConnection();
+  const result = await productGetEdit.queryGetProductById(pool, id, productId);
+  await pool.end();
   response.status(200).json(result);
 };
 
 const deleteProduct = async (request, response) => {
   const { id } = request.params;
-  const deletedTheSupplies = await suppliesDelete.queryDeleteSuppliesFromProduct(pool, id);
-  const deletedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(pool, id);
+  const pool = await getDbConnection();
+  const deletedTheSupplies =
+    await suppliesDelete.queryDeleteSuppliesFromProduct(pool, id);
+  const deletedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(
+    pool,
+    id
+  );
   const deletedProduct = await productDelete.queryDeleteProduct(pool, id);
 
   console.log("delete supplies: " + deletedTheSupplies);
@@ -53,73 +65,78 @@ const deleteProduct = async (request, response) => {
   console.log("delete product: " + deletedProduct);
 
   const myResult = deletedProduct;
-
-  response.send({ status: (myResult ? 'OK' : 'NOK') });
+  await pool.end();
+  response.send({ status: myResult ? "OK" : "NOK" });
 };
 
 const getProducts = async (request, response) => {
   const id = extractToken(request);
-  const list = await productGet.queryGetProduct(pool, id);
+  const db = await getDbConnection();
+  const list = await productGet.queryGetProduct(db, id);
+  await db.end();
   response.status(200).json(list);
 };
 
-
 const createProduct = async (request, response) => {
   const { supplies, recipes } = request.body;
+  const db = await getDbConnection();
 
-  const resultProductId = await productAdd.queryAddProduct(pool, request.body);
-  var result = await suppliesAdd.queryAddSupplies(pool, resultProductId, supplies);
-  const result2 = await recipeAdd.queryAddRecipes(pool, resultProductId, recipes);
+  const resultProductId = await productAdd.queryAddProduct(db, request.body);
+  const result = await suppliesAdd.queryAddSupplies(db, resultProductId, supplies);
+  const result2 = await recipeAdd.queryAddRecipes(db, resultProductId, recipes);
 
   if (result && !result2) {
-
-    const deletedTheSupplies = await suppliesDelete.queryDeleteSuppliesFromProduct(pool, resultProductId);
-    const deletedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(pool, resultProductId);
-    const deletedProduct = await productDelete.queryDeleteProduct(pool, resultProductId);
-
+    const deletedTheSupplies = await suppliesDelete.queryDeleteSuppliesFromProduct(db, resultProductId);
+    const deletedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(db, resultProductId);
+    const deletedProduct = await productDelete.queryDeleteProduct(db, resultProductId);
+    await db.end();
     response.status(200).json({
-      status: 'NOK', message: "internal error",
+      status: "NOK",
+      message: "internal error",
       deletedSupplies: deletedTheSupplies,
       deletedRecipes: deletedRecipes,
-      deletedProduct: deletedProduct
+      deletedProduct: deletedProduct,
     });
-
   } else {
-    response.status(200).json({ status: (result && result2 ? 'OK' : 'NOK') });
+    await db.end();
+    response.status(200).json({ status: result && result2 ? "OK" : "NOK" });
   }
-
 };
-
 
 const updateProduct = async (request, response) => {
   const { id } = request.params;
   const body = request.body;
   const { supplies, recipes } = request.body;
+  const pool = await getDbConnection();
   var result = await productUpdate.queryUpdateProducts(pool, body, id);
 
   if (result === true) {
-
-    var hasRemovedSupplies = await suppliesDelete.queryDeleteSuppliesFromProduct(pool, id);
+    var hasRemovedSupplies =
+      await suppliesDelete.queryDeleteSuppliesFromProduct(pool, id);
     console.log("hasRemovedSupplies: " + hasRemovedSupplies);
     var result1 = await suppliesAdd.queryAddSupplies(pool, id, supplies);
     console.log("has added supplies: " + result1);
 
-    var hasRemovedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(pool, id);
+    var hasRemovedRecipes = await recipeDelete.queryDeleteRecipesFromProduct(
+      pool,
+      id
+    );
     console.log("hasRemovedRecipes: " + hasRemovedRecipes);
     var result2 = await recipeAdd.queryAddRecipes(pool, id, recipes);
     console.log("has added recipes: " + result2);
 
-    response.send({ status: 'OK' });
+    await pool.end();
+    response.send({ status: "OK" });
   } else {
-    response.send({ status: 'NOK' });
+    response.send({ status: "NOK" });
   }
 };
 
 // Webhooks - Recipe
 
 /**
- * This method will update a recipe. 
- * First, it will get all recipes by the remote id and then: 
+ * This method will update a recipe.
+ * First, it will get all recipes by the remote id and then:
  *  - Remove old data
  *  - Save old data such as product_id and quantity
  *  - Add the new recipe along with old row data
@@ -134,11 +151,11 @@ const updateRecipe = async (request, response) => {
   var list = await recipeGet.queryGetRecipes(pool, id, userId);
   var result = await recipeRecal.recalRecipe(pool, body, userId, list);
 
-  if(result) {
+  if (result) {
     recal.executeRecalculate(pool, userId);
   }
 
-  response.status(200).json({ status: (result ? 'OK' : 'NOK') });
+  response.status(200).json({ status: result ? "OK" : "NOK" });
 };
 
 /**
@@ -149,14 +166,18 @@ const updateRecipe = async (request, response) => {
 const deleteRecipe = async (request, response) => {
   const recipeId = request.body.id;
   const userId = extractToken(request);
-  var hasDeleted = await recipeDelete.queryDeleteRecipeWith(pool, recipeId, userId);
+  var hasDeleted = await recipeDelete.queryDeleteRecipeWith(
+    pool,
+    recipeId,
+    userId
+  );
   console.log("deleteRecipe: " + hasDeleted);
 
-  if(hasDeleted) {
+  if (hasDeleted) {
     recal.executeRecalculate(pool, userId);
   }
 
-  response.status(200).json({ status: (hasDeleted ? 'OK' : 'NOK') });
+  response.status(200).json({ status: hasDeleted ? "OK" : "NOK" });
 };
 
 // Webhooks - Supply
@@ -173,11 +194,11 @@ const updateSupply = async (request, response) => {
   var hasUpdated = await suppliesUpdate.updateSupplies(pool, supply, userId);
   console.log("updateSupply: " + hasUpdated);
 
-  if(hasUpdated) {
+  if (hasUpdated) {
     recal.executeRecalculate(pool, userId);
   }
 
-  response.status(200).json({ status: (hasUpdated ? 'OK' : 'NOK') });
+  response.status(200).json({ status: hasUpdated ? "OK" : "NOK" });
 };
 
 /**
@@ -188,30 +209,42 @@ const updateSupply = async (request, response) => {
 const deleteSupply = async (request, response) => {
   const supplyId = request.body.id;
   const userId = extractToken(request);
-  var hasDeleted = await suppliesDelete.queryDeleteSupplyByRemoteId(pool, supplyId, userId);
+  var hasDeleted = await suppliesDelete.queryDeleteSupplyByRemoteId(
+    pool,
+    supplyId,
+    userId
+  );
   console.log("deleteSupply: " + hasDeleted);
 
-  if(hasDeleted) {
+  if (hasDeleted) {
     recal.executeRecalculate(pool, userId);
   }
 
-  response.status(200).json({ status: (hasDeleted ? 'OK' : 'NOK') });
+  response.status(200).json({ status: hasDeleted ? "OK" : "NOK" });
 };
 
 const recalculate = async (request, response) => {
   const userId = extractToken(request);
   var result = await recal.executeRecalculate(pool, userId);
-  response.status(200).json({ status: (result ? 'OK' : 'NOK') });
+  response.status(200).json({ status: result ? "OK" : "NOK" });
 };
 
 const deleteAll = async (request, response) => {
-  var userId  = request.body.userId;
+  var userId = request.body.userId;
   var result = await deleteAllPro.executeDeleteAll(pool, userId);
-  response.status(200).json({ status: (result ? 'OK' : 'NOK') });
+  response.status(200).json({ status: result ? "OK" : "NOK" });
 };
 
 module.exports = {
-  getProducts, createProduct, deleteProduct, getProductGetEdit,
-  updateProduct, updateRecipe, deleteRecipe, updateSupply, deleteSupply,
-  recalculate, deleteAll
-}
+  getProducts,
+  createProduct,
+  deleteProduct,
+  getProductGetEdit,
+  updateProduct,
+  updateRecipe,
+  deleteRecipe,
+  updateSupply,
+  deleteSupply,
+  recalculate,
+  deleteAll,
+};
