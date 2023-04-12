62ab35f278b2eef4e0bacacc76b701f6dc571e49
Diogo Ribeiro <diogjp10@gmail.com>
2023-04-12 12:03:28 +0100

    fix recal

--- revenue-tax-get-util.js
+++ revenue-tax-get-util.js
@@ -1,6 +1,23 @@
 function calculateMarkup(expenses, revenue) {
-    var result = Number((expenses / revenue) * 100);
-    return result;
+  try {
+    var newExpense = 0.0;
+    if (!isNaN(expenses)) {
+      newExpense = Number(expenses);
+    }
+
+    if (isNaN(revenue)) {
+      return 0;
+    }
+
+    var result = Number((newExpense / revenue) * 100);
+    if (isNaN(result)) {
+      return 0;
+    } else {
+      return result;
+    }
+  } catch (error) {
+    return 0;
+  }
 }
 
-module.exports = { calculateMarkup }
+module.exports = { calculateMarkup };
--- test/revenue-tax-get-util.test.js
+++ test/revenue-tax-get-util.test.js
@@ -3,8 +3,18 @@ const util = require("../revenue-tax-get-util");
 
 describe("Should validate cal markup", () => {
   it("should be 100", () => {
-    assert.strictEqual(util.calculateMarkup(10,10), 10);
+    assert.strictEqual(util.calculateMarkup(10, 10), 100);
   });
 
-  
+  it("should handle Nan on expenses", () => {
+    assert.strictEqual(util.calculateMarkup(NaN, 10), 0);
+  });
+
+  it("should handle Nan on revenue", () => {
+    assert.strictEqual(util.calculateMarkup(10, NaN), 0);
+  });
+
+  it("should handle zero on all", () => {
+    assert.strictEqual(util.calculateMarkup(0, 0), 0);
+  });
 });
