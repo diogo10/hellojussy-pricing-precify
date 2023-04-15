bbc886d2523c8048eb159d9f07b0f3165b6bb851
Diogo Ribeiro <diogjp10@gmail.com>
2023-04-15 13:32:39 +0100

    update urls

---
+++ revenue-tax-get-util.js
@@ -0,0 +1,23 @@
+function calculateMarkup(expenses, revenue) {
+    try {
+      var newExpense = 0.0;
+      if (!isNaN(expenses)) {
+        newExpense = Number(expenses);
+      }
+ 
+      if (isNaN(revenue)) {
+        return 0;
+      }
+ 
+      var result = Number((newExpense / revenue) * 100);
+      if (isNaN(result)) {
+        return 0;
+      } else {
+        return result;
+      }
+    } catch (error) {
+      return 0;
+    }
+  }
+ 
+  module.exports = { calculateMarkup };
